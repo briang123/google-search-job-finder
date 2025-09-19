a3ba932fe99cdc652c9f6572f18a919571e48260
Brian Gaines <briang.sandiego@gmail.com>
2025-09-19 15:07:37 -0700

    analytics

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,11 @@
-import { Switch, Route } from "wouter";
-import { queryClient } from "./lib/queryClient";
-import { QueryClientProvider } from "@tanstack/react-query";
-import { Toaster } from "@/components/ui/toaster";
-import { TooltipProvider } from "@/components/ui/tooltip";
-import Home from "@/pages/home";
-import NotFound from "@/pages/not-found";
+import { Switch, Route } from 'wouter';
+import { queryClient } from './lib/queryClient';
+import { QueryClientProvider } from '@tanstack/react-query';
+import { Toaster } from '@/components/ui/toaster';
+import { TooltipProvider } from '@/components/ui/tooltip';
+import { Analytics } from '@vercel/analytics/react';
+import Home from '@/pages/home';
+import NotFound from '@/pages/not-found';
 
 function Router() {
   return (
@@ -19,6 +20,7 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <TooltipProvider>
+        <Analytics />
         <Toaster />
         <Router />
       </TooltipProvider>
